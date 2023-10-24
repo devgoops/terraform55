7f9c2389f7d1ef2c5e0ae6deb63ecd4e39d16353
seneca <devgoops>
2023-10-24 08:47:00 -0300

    adding-datasource

--- TERRAFORM012/dependencies/main.tf
+++ TERRAFORM012/dependencies/main.tf
@@ -5,6 +5,10 @@ provider "aws" {
 resource "aws_instance" "db" {
   ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"
+
+  tags = {
+    name = "Db Server"
+  }
 }
 
 resource "aws_instance" "web" {
@@ -12,3 +16,17 @@ resource "aws_instance" "web" {
   ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"
 }
+
+
+# data "aws_instance" "db_search" {
+#   filter {
+#     name = "tag:Name"
+#     values = ["Db Server"]
+#   }
+# }
+
+# output "dbserver" {
+#   value = data.aws_instance.db_search.availability_zone
+# }
+
+
